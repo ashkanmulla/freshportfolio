d0bf7e31ebf75c5ee467060b95518aaf97eed747
Askan Mulla <askan.mulla.ext@lanxess.com>
2025-06-04 14:55:17 +0530

    Enhance mobile responsiveness in hero section and improve smooth scrolling behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,27 +30,20 @@ $(document).ready(function () {
     });
 
     // smooth scrolling
+// smooth scrolling
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top,
-        }, 500, 'linear')
-    });
-
-    // <!-- emailjs to mail contact form data -->
-    $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
-
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
-            });
-        event.preventDefault();
+        let target = $($(this).attr('href'));
+        if (target.length) {
+            let scrollTo = target.offset().top;
+            // Adjust for mobile
+            if (window.innerWidth <= 768) {
+                scrollTo -= 65;
+            }
+            $('html, body').animate({
+                scrollTop: scrollTo
+            }, 500, 'linear');
+        }
     });
     // <!-- emailjs to mail contact form data -->
 
